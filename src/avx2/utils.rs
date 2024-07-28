426459151fecf72ec62cba2f21b211f81f0afef4
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2024-07-29 00:05:29 +0100

    Added vertical u16

--- src/avx2/utils.rs
+++ src/avx2/utils.rs
@@ -27,7 +27,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-use crate::support::PRECISION;
 #[cfg(target_arch = "x86")]
 use std::arch::x86::*;
 #[cfg(target_arch = "x86_64")]
