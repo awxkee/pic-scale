a6af6d483bc3a2ae598567c8bd3241bedbdabdf1
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-05-02 16:22:38 +0100

    Refactor avx512

--- src/avx512/vertical_u16_lb.rs
+++ src/avx512/vertical_u16_lb.rs
@@ -63,7 +63,8 @@ unsafe fn convolve_column_lb_avx512_reg(
     weight: &[i16],
     bit_depth: u32,
 ) {
-    convolve_column_lb_avx512_impl::<false>(bounds, src, dst, src_stride, weight, bit_depth);
+    let unit = ExecutionUnit::<false>::default();
+    unit.pass(bounds, src, dst, src_stride, weight, bit_depth);
 }
 
 #[target_feature(enable = "avx512f", enable = "avx512bw", enable = "avx512vnni")]
@@ -75,213 +76,229 @@ unsafe fn convolve_column_lb_avx512_dot(
     weight: &[i16],
     bit_depth: u32,
 ) {
-    convolve_column_lb_avx512_impl::<true>(bounds, src, dst, src_stride, weight, bit_depth);
+    let unit = ExecutionUnit::<true>::default();
+    unit.pass(bounds, src, dst, src_stride, weight, bit_depth);
 }
 
-#[inline(always)]
-unsafe fn convolve_column_lb_avx512_impl<const HAS_DOT: bool>(
-    bounds: &FilterBounds,
-    src: &[u16],
-    dst: &mut [u16],
-    src_stride: usize,
-    weight: &[i16],
-    bit_depth: u32,
-) {
-    assert!((1..=16).contains(&bit_depth));
-    let max_colors = (1 << bit_depth) - 1;
+#[derive(Copy, Clone, Default)]
+struct ExecutionUnit<const HAS_DOT: bool> {}
 
-    let bounds_size = bounds.size;
+impl<const HAS_DOT: bool> ExecutionUnit<HAS_DOT> {
+    #[inline(always)]
+    unsafe fn pass(
+        &self,
+        bounds: &FilterBounds,
+        src: &[u16],
+        dst: &mut [u16],
+        src_stride: usize,
+        weight: &[i16],
+        bit_depth: u32,
+    ) {
+        assert!((1..=16).contains(&bit_depth));
+        let max_colors = (1 << bit_depth) - 1;
 
-    let zeros = _mm512_setzero_si512();
-    let initial_store = _mm512_set1_epi32(ROUNDING_CONST);
-    let v_max_colors = _mm512_set1_epi16(max_colors);
+        let bounds_size = bounds.size;
 
-    const PRECISION: u32 = 15;
+        let zeros = _mm512_setzero_si512();
+        let initial_store = _mm512_set1_epi32(ROUNDING_CONST);
+        let v_max_colors = _mm512_set1_epi16(max_colors);
 
-    for (x, dst) in dst.chunks_mut(32).enumerate() {
-        let mut store0 = initial_store;
-        let mut store1 = initial_store;
+        const PRECISION: u32 = 15;
 
-        let v_dx = x * 32;
+        for (x, dst) in dst.chunks_mut(32).enumerate() {
+            let mut store0 = initial_store;
+            let mut store1 = initial_store;
 
-        let working_mask: __mmask32 = if dst.len() == 32 {
-            0xffff_ffff
-        } else {
-            0xffff_ffff >> (32 - dst.len())
-        };
+            let v_dx = x * 32;
 
-        if bounds_size == 2 {
-            let weights = weight.get_unchecked(0..2);
+            let working_mask: __mmask32 = if dst.len() == 32 {
+                0xffff_ffff
+            } else {
+                0xffff_ffff >> (32 - dst.len())
+            };
 
-            let v_weight0 = _mm512_set1_epi16(weights[0]);
-            let v_weight1 = _mm512_set1_epi16(weights[1]);
+            if bounds_size == 2 {
+                let weights = weight.get_unchecked(0..2);
 
-            let py = bounds.start;
-            let src_ptr0 = src.get_unchecked((src_stride * py + v_dx)..);
-            let src_ptr1 = src.get_unchecked((src_stride * (py + 1) + v_dx)..);
+                let v_weight0 = _mm512_set1_epi16(weights[0]);
+                let v_weight1 = _mm512_set1_epi16(weights[1]);
 
-            let item_row0 = _mm512_maskz_loadu_epi16(working_mask, src_ptr0.as_ptr() as *const i16);
-            let item_row1 = _mm512_maskz_loadu_epi16(working_mask, src_ptr1.as_ptr() as *const i16);
-
-            store0 = _mm512_dot16_epi32::<HAS_DOT>(
-                store0,
-                _mm512_unpacklo_epi16(item_row0, zeros),
-                v_weight0,
-            );
-            store1 = _mm512_dot16_epi32::<HAS_DOT>(
-                store1,
-                _mm512_unpackhi_epi16(item_row0, zeros),
-                v_weight0,
-            );
-
-            store0 = _mm512_dot16_epi32::<HAS_DOT>(
-                store0,
-                _mm512_unpacklo_epi16(item_row1, zeros),
-                v_weight1,
-            );
-            store1 = _mm512_dot16_epi32::<HAS_DOT>(
-                store1,
-                _mm512_unpackhi_epi16(item_row1, zeros),
-                v_weight1,
-            );
-        } else if bounds_size == 3 {
-            let weights = weight.get_unchecked(0..3);
-
-            let v_weight0 = _mm512_set1_epi16(weights[0]);
-            let v_weight1 = _mm512_set1_epi16(weights[1]);
-            let v_weight2 = _mm512_set1_epi16(weights[2]);
-
-            let py = bounds.start;
-            let src_ptr0 = src.get_unchecked((src_stride * py + v_dx)..);
-            let src_ptr1 = src.get_unchecked((src_stride * (py + 1) + v_dx)..);
-            let src_ptr2 = src.get_unchecked((src_stride * (py + 2) + v_dx)..);
-
-            let item_row0 = _mm512_maskz_loadu_epi16(working_mask, src_ptr0.as_ptr() as *const i16);
-            let item_row1 = _mm512_maskz_loadu_epi16(working_mask, src_ptr1.as_ptr() as *const i16);
-            let item_row2 = _mm512_maskz_loadu_epi16(working_mask, src_ptr2.as_ptr() as *const i16);
-
-            store0 = _mm512_dot16_epi32::<HAS_DOT>(
-                store0,
-                _mm512_unpacklo_epi16(item_row0, zeros),
-                v_weight0,
-            );
-            store1 = _mm512_dot16_epi32::<HAS_DOT>(
-                store1,
-                _mm512_unpackhi_epi16(item_row0, zeros),
-                v_weight0,
-            );
-
-            store0 = _mm512_dot16_epi32::<HAS_DOT>(
-                store0,
-                _mm512_unpacklo_epi16(item_row1, zeros),
-                v_weight1,
-            );
-            store1 = _mm512_dot16_epi32::<HAS_DOT>(
-                store1,
-                _mm512_unpackhi_epi16(item_row1, zeros),
-                v_weight1,
-            );
-
-            store0 = _mm512_dot16_epi32::<HAS_DOT>(
-                store0,
-                _mm512_unpacklo_epi16(item_row2, zeros),
-                v_weight2,
-            );
-            store1 = _mm512_dot16_epi32::<HAS_DOT>(
-                store1,
-                _mm512_unpackhi_epi16(item_row2, zeros),
-                v_weight2,
-            );
-        } else if bounds_size == 4 {
-            let weights = weight.get_unchecked(0..4);
-
-            let v_weight0 = _mm512_set1_epi16(weights[0]);
-            let v_weight1 = _mm512_set1_epi16(weights[1]);
-            let v_weight2 = _mm512_set1_epi16(weights[2]);
-            let v_weight3 = _mm512_set1_epi16(weights[3]);
-
-            let py = bounds.start;
-            let src_ptr0 = src.get_unchecked((src_stride * py + v_dx)..);
-            let src_ptr1 = src.get_unchecked((src_stride * (py + 1) + v_dx)..);
-            let src_ptr2 = src.get_unchecked((src_stride * (py + 2) + v_dx)..);
-            let src_ptr3 = src.get_unchecked((src_stride * (py + 3) + v_dx)..);
-
-            let item_row0 = _mm512_maskz_loadu_epi16(working_mask, src_ptr0.as_ptr() as *const i16);
-            let item_row1 = _mm512_maskz_loadu_epi16(working_mask, src_ptr1.as_ptr() as *const i16);
-            let item_row2 = _mm512_maskz_loadu_epi16(working_mask, src_ptr2.as_ptr() as *const i16);
-            let item_row3 = _mm512_maskz_loadu_epi16(working_mask, src_ptr3.as_ptr() as *const i16);
-
-            store0 = _mm512_dot16_epi32::<HAS_DOT>(
-                store0,
-                _mm512_unpacklo_epi16(item_row0, zeros),
-                v_weight0,
-            );
-            store1 = _mm512_dot16_epi32::<HAS_DOT>(
-                store1,
-                _mm512_unpackhi_epi16(item_row0, zeros),
-                v_weight0,
-            );
-
-            store0 = _mm512_dot16_epi32::<HAS_DOT>(
-                store0,
-                _mm512_unpacklo_epi16(item_row1, zeros),
-                v_weight1,
-            );
-            store1 = _mm512_dot16_epi32::<HAS_DOT>(
-                store1,
-                _mm512_unpackhi_epi16(item_row1, zeros),
-                v_weight1,
-            );
-
-            store0 = _mm512_dot16_epi32::<HAS_DOT>(
-                store0,
-                _mm512_unpacklo_epi16(item_row2, zeros),
-                v_weight2,
-            );
-            store1 = _mm512_dot16_epi32::<HAS_DOT>(
-                store1,
-                _mm512_unpackhi_epi16(item_row2, zeros),
-                v_weight2,
-            );
-
-            store0 = _mm512_dot16_epi32::<HAS_DOT>(
-                store0,
-                _mm512_unpacklo_epi16(item_row3, zeros),
-                v_weight3,
-            );
-            store1 = _mm512_dot16_epi32::<HAS_DOT>(
-                store1,
-                _mm512_unpackhi_epi16(item_row3, zeros),
-                v_weight3,
-            );
-        } else {
-            for (j, &k_weight) in weight.iter().take(bounds_size).enumerate() {
-                let py = bounds.start + j;
-                let src_ptr = src.get_unchecked((src_stride * py + v_dx)..);
-
-                let v_weight = _mm512_set1_epi16(k_weight);
+                let py = bounds.start;
+                let src_ptr0 = src.get_unchecked((src_stride * py + v_dx)..);
+                let src_ptr1 = src.get_unchecked((src_stride * (py + 1) + v_dx)..);
 
                 let item_row0 =
-                    _mm512_maskz_loadu_epi16(working_mask, src_ptr.as_ptr() as *const i16);
+                    _mm512_maskz_loadu_epi16(working_mask, src_ptr0.as_ptr() as *const i16);
+                let item_row1 =
+                    _mm512_maskz_loadu_epi16(working_mask, src_ptr1.as_ptr() as *const i16);
 
                 store0 = _mm512_dot16_epi32::<HAS_DOT>(
                     store0,
                     _mm512_unpacklo_epi16(item_row0, zeros),
-                    v_weight,
+                    v_weight0,
                 );
                 store1 = _mm512_dot16_epi32::<HAS_DOT>(
                     store1,
                     _mm512_unpackhi_epi16(item_row0, zeros),
-                    v_weight,
+                    v_weight0,
                 );
+
+                store0 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store0,
+                    _mm512_unpacklo_epi16(item_row1, zeros),
+                    v_weight1,
+                );
+                store1 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store1,
+                    _mm512_unpackhi_epi16(item_row1, zeros),
+                    v_weight1,
+                );
+            } else if bounds_size == 3 {
+                let weights = weight.get_unchecked(0..3);
+
+                let v_weight0 = _mm512_set1_epi16(weights[0]);
+                let v_weight1 = _mm512_set1_epi16(weights[1]);
+                let v_weight2 = _mm512_set1_epi16(weights[2]);
+
+                let py = bounds.start;
+                let src_ptr0 = src.get_unchecked((src_stride * py + v_dx)..);
+                let src_ptr1 = src.get_unchecked((src_stride * (py + 1) + v_dx)..);
+                let src_ptr2 = src.get_unchecked((src_stride * (py + 2) + v_dx)..);
+
+                let item_row0 =
+                    _mm512_maskz_loadu_epi16(working_mask, src_ptr0.as_ptr() as *const i16);
+                let item_row1 =
+                    _mm512_maskz_loadu_epi16(working_mask, src_ptr1.as_ptr() as *const i16);
+                let item_row2 =
+                    _mm512_maskz_loadu_epi16(working_mask, src_ptr2.as_ptr() as *const i16);
+
+                store0 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store0,
+                    _mm512_unpacklo_epi16(item_row0, zeros),
+                    v_weight0,
+                );
+                store1 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store1,
+                    _mm512_unpackhi_epi16(item_row0, zeros),
+                    v_weight0,
+                );
+
+                store0 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store0,
+                    _mm512_unpacklo_epi16(item_row1, zeros),
+                    v_weight1,
+                );
+                store1 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store1,
+                    _mm512_unpackhi_epi16(item_row1, zeros),
+                    v_weight1,
+                );
+
+                store0 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store0,
+                    _mm512_unpacklo_epi16(item_row2, zeros),
+                    v_weight2,
+                );
+                store1 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store1,
+                    _mm512_unpackhi_epi16(item_row2, zeros),
+                    v_weight2,
+                );
+            } else if bounds_size == 4 {
+                let weights = weight.get_unchecked(0..4);
+
+                let v_weight0 = _mm512_set1_epi16(weights[0]);
+                let v_weight1 = _mm512_set1_epi16(weights[1]);
+                let v_weight2 = _mm512_set1_epi16(weights[2]);
+                let v_weight3 = _mm512_set1_epi16(weights[3]);
+
+                let py = bounds.start;
+                let src_ptr0 = src.get_unchecked((src_stride * py + v_dx)..);
+                let src_ptr1 = src.get_unchecked((src_stride * (py + 1) + v_dx)..);
+                let src_ptr2 = src.get_unchecked((src_stride * (py + 2) + v_dx)..);
+                let src_ptr3 = src.get_unchecked((src_stride * (py + 3) + v_dx)..);
+
+                let item_row0 =
+                    _mm512_maskz_loadu_epi16(working_mask, src_ptr0.as_ptr() as *const i16);
+                let item_row1 =
+                    _mm512_maskz_loadu_epi16(working_mask, src_ptr1.as_ptr() as *const i16);
+                let item_row2 =
+                    _mm512_maskz_loadu_epi16(working_mask, src_ptr2.as_ptr() as *const i16);
+                let item_row3 =
+                    _mm512_maskz_loadu_epi16(working_mask, src_ptr3.as_ptr() as *const i16);
+
+                store0 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store0,
+                    _mm512_unpacklo_epi16(item_row0, zeros),
+                    v_weight0,
+                );
+                store1 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store1,
+                    _mm512_unpackhi_epi16(item_row0, zeros),
+                    v_weight0,
+                );
+
+                store0 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store0,
+                    _mm512_unpacklo_epi16(item_row1, zeros),
+                    v_weight1,
+                );
+                store1 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store1,
+                    _mm512_unpackhi_epi16(item_row1, zeros),
+                    v_weight1,
+                );
+
+                store0 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store0,
+                    _mm512_unpacklo_epi16(item_row2, zeros),
+                    v_weight2,
+                );
+                store1 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store1,
+                    _mm512_unpackhi_epi16(item_row2, zeros),
+                    v_weight2,
+                );
+
+                store0 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store0,
+                    _mm512_unpacklo_epi16(item_row3, zeros),
+                    v_weight3,
+                );
+                store1 = _mm512_dot16_epi32::<HAS_DOT>(
+                    store1,
+                    _mm512_unpackhi_epi16(item_row3, zeros),
+                    v_weight3,
+                );
+            } else {
+                for (j, &k_weight) in weight.iter().take(bounds_size).enumerate() {
+                    let py = bounds.start + j;
+                    let src_ptr = src.get_unchecked((src_stride * py + v_dx)..);
+
+                    let v_weight = _mm512_set1_epi16(k_weight);
+
+                    let item_row0 =
+                        _mm512_maskz_loadu_epi16(working_mask, src_ptr.as_ptr() as *const i16);
+
+                    store0 = _mm512_dot16_epi32::<HAS_DOT>(
+                        store0,
+                        _mm512_unpacklo_epi16(item_row0, zeros),
+                        v_weight,
+                    );
+                    store1 = _mm512_dot16_epi32::<HAS_DOT>(
+                        store1,
+                        _mm512_unpackhi_epi16(item_row0, zeros),
+                        v_weight,
+                    );
+                }
             }
+
+            let v_st0 = _mm512_srai_epi32::<PRECISION>(store0);
+            let v_st1 = _mm512_srai_epi32::<PRECISION>(store1);
+
+            let item0 = _mm512_min_epi16(_mm512_packus_epi32(v_st0, v_st1), v_max_colors);
+
+            _mm512_mask_storeu_epi16(dst.as_mut_ptr() as *mut i16, working_mask, item0);
         }
-
-        let v_st0 = _mm512_srai_epi32::<PRECISION>(store0);
-        let v_st1 = _mm512_srai_epi32::<PRECISION>(store1);
-
-        let item0 = _mm512_min_epi16(_mm512_packus_epi32(v_st0, v_st1), v_max_colors);
-
-        _mm512_mask_storeu_epi16(dst.as_mut_ptr() as *mut i16, working_mask, item0);
     }
 }
